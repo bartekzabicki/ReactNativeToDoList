90bc8caaae0be5b0c476d3400b8963230fce95d9
Bartek Żabicki <bartekzabicki@gmail.com>
2018-06-11 13:16:35 +0200

    Add swipe options

--- src/components/controllers/Dashboard/List/TaskCellComponent/TaskCellComponent.js
+++ src/components/controllers/Dashboard/List/TaskCellComponent/TaskCellComponent.js
@@ -5,14 +5,32 @@ import {
     StyleSheet
     } from "react-native"
 ;
+import Swipeout from 'react-native-swipeout';
 
 export default class TaskCellComponent extends Component{
     render(){
+        
+        let swipeBtns = [{
+          text: 'Delete',
+          backgroundColor: 'red',
+          underlayColor: 'rgba(0, 0, 0, 1, 0.6)',
+          onPress: () => { }
+        },{
+            text: 'Edit',
+            backgroundColor: 'blue',
+            underlayColor: 'rgba(0, 0, 0, 1, 0.6)',
+            onPress: () => { }
+          }];
+
         return (
+            <Swipeout right={swipeBtns}
+              autoClose='true'
+              backgroundColor= 'transparent'>
             <View style={styles.container}>
                 <Text style={{fontSize: 17, fontWeight: '500'}}>{this.props.title}</Text>
                 <Text style={{fontSize: 12}}>{this.props.subtitle}</Text>
             </View>
+      </Swipeout>
         );
     }
 }
@@ -20,7 +38,6 @@ export default class TaskCellComponent extends Component{
 const styles = StyleSheet.create({
     container:{
     flex:1,
-    alignItems:'left',
     justifyContent:'left',
     marginLeft: 24,
     marginTop: 4,
